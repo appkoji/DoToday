61be992d0d0d77d8111a198a16315c232bdff604
Koji Murata <appkojidev@gmail.com>
2023-08-21 13:34:13 +0800

    Initial Commit

---
+++ DoToday/DoTodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoTodayApp.swift
+//  DoToday
+//
+//  Created by 村田興児 on 2023/08/21.
+//
+
+import SwiftUI
+
+@main
+struct DoTodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
